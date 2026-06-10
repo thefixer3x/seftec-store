5f927ee3ccdaa6ae8c42128f088b3c525258764e
The Fixer <info@lanonasis.com>
2026-06-10 05:05:03 +0100

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => {
   const enableSentryBuild = mode !== 'development' && Boolean(sentryAuthToken);
 
   return ({
+  root: __dirname,
   server: {
     host: "127.0.0.1",
     port: 8080,
